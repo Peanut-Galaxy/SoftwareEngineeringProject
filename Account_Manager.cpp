c99ac43902d5e0281aa05de1be1e6be66714c873
Peanut-Galaxy <54990961+Peanut-Galaxy@users.noreply.github.com>
2020-04-27 13:52:43 -0500

    Minor refactoring
    Split the Account class into its own file separate from the file containing main()

--- Account_Manager.cpp
+++ Account_Manager.cpp
@@ -3,98 +3,13 @@
 // MAIN is at the bottom
 
 // Windows #include "stdafx.h"
-#include<iostream>
-#include<fstream>
-#include<cctype>
-#include<iomanip>
+#include <iostream>
+#include <fstream>
+#include <cctype>
+#include <iomanip>
+#include "Account.cpp"
 
 using namespace std;
-
-// Account class
-class Account
-{
-int acno;
-char name[50];
-int deposit;
-char type;
-public:
-void create_account(); //function to get data from user
-void show_account() const; //function to show data on screen
-void modify(); //function to add new data
-void dep(int); //function to accept amount and add to balance amount
-void draw(int); //function to accept amount and subtract from balance amount
-void report() const; //function to show data in tabular format
-int retacno() const; //function to return account number
-int retdeposit() const; //function to return balance amount
-char rettype() const; //function to return type of account
-};         //class ends here
-
-void Account::create_account()
-{
-cout <<"\nEnter The account No. : ";
-cin >> acno;
-cout << "\n\nEnter The Name of The account Holder : ";
-cin.ignore();
-cin.getline(name,50);
-cout << "\nEnter Type of The account (C/S) : ";
-cin >> type;
-type = toupper(type);
-cout << "\nEnter The Initial amount(>=500 for Saving and >=1000 for current ) : ";
-cin >> deposit;
-cout << "\n\n\nAccount Created..";
-}
-
-void Account::show_account() const
-{
-cout << "\nAccount No. : "<< acno;
-cout << "\nAccount Holder Name : ";
-cout << name;
-cout << "\nType of Account : "<< type;
-cout << "\nBalance amount : "<< deposit;
-}
-
-void Account::modify()
-{
-cout << "\nAccount No. : "<<acno;
-cout << "\nModify Account Holder Name : ";
-cin.ignore();
-cin.getline(name,50);
-cout << "\nModify Type of Account : ";
-cin >> type;
-type = toupper(type);
-cout << "\nModify Balance amount : ";
-cin >> deposit;
-}
-
-void Account::dep(int x)
-{
-deposit += x;
-}
-
-void Account::draw(int x)
-{
-deposit -= x;
-}
-
-void Account::report() const
-{
-cout << acno << setw(10) << " " << name << setw(10) << " " << type << setw(6) << deposit << endl;
-}
-
-int Account::retacno() const
-{
-return acno;
-}
-
-int Account::retdeposit() const
-{
-return deposit;
-}
-
-char Account::rettype() const
-{
-return type;
-}
 
 // Global functions
 
